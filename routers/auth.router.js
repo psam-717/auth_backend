e409bc00e34ffa401f55e59b48a2a8e746267c22
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-03-11 23:10:26 +0000

    router for sending forgot password code, verifying the code and updating the password

--- routers/auth.router.js
+++ routers/auth.router.js
@@ -9,6 +9,8 @@ router.post('/login', authController.login);
 router.post('/logout', authenticateToken ,authController.logout);
 router.patch('/send_verification_code', authenticateToken ,authController.sendVerificationCode); 
 router.patch('/verify_verification_code', authenticateToken ,authController.verifyVerificationCode);
-router.get('/home', authenticateToken, authController.home)
-router.patch('/change_password', authenticateToken, authController.changePassword)
+router.get('/home', authenticateToken, authController.home);
+router.patch('/change_password', authenticateToken, authController.changePassword);
+router.patch('/send_forgot_password_code', authenticateToken, authController.sendForgotPasswordCode);
+router.patch('/verify_forgot_password_code', authenticateToken, authController.verifyForgotPasswordCode);
 module.exports = router;
