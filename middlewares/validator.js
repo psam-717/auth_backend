9c7816865f9b2f780f09da1b5e96889f88a39fe8
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-03-14 15:24:49 +0000

    included the schemas for posts

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -66,5 +66,36 @@ exports.verifyForgotPasswordCodeSchema = Joi.object({
                     .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/)
                     .message('Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit')
 
+})
+
+exports.makePostsSchema = Joi.object({
+    title: Joi.string().min(2).required().messages({
+        'string.empty': 'Title is required',
+        'string.min': 'Title is must be at least to characters long',
+    }),
+    category: Joi.string().min(2).required().messages({
+        'string.empty':'Category is required',
+        'string.min':'Category must be at least two characters long'
+    }),
+    description: Joi.string().min(2).required().messages({
+        'string.empty': 'description is required',
+        'string.min': 'description must be at least two characters long'
+    })
 
 })
+
+
+exports.updatePostsSchema = Joi.object({
+    title: Joi.string().min(2).messages({
+        'string.empty': 'Title is required',
+        'string.min': 'Title is must be at least to characters long',
+    }),
+    category: Joi.string().min(2).messages({
+        'string.empty':'Category is required',
+        'string.min':'Category must be at least two characters long'
+    }),
+    description: Joi.string().min(2).messages({
+        'string.empty': 'description is required',
+        'string.min': 'description must be at least two characters long'
+    })
+}).or('title', 'description', 'category')
